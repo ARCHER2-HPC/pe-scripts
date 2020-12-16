9498d6e6c7adf3a0304709c2fd69e7350feb4ac1
Kevin Stratford <kevin@epcc.ed.ac.uk>
2020-12-16 15:10:49 +0000

    Add METIS_DIR in template

--- archer2/tpsl/metis/modulefile.tcl
+++ archer2/tpsl/metis/modulefile.tcl
@@ -1,7 +1,7 @@
 #%Module
 #
 
-# You must replace:
+# We must replace:
 # TEMPLATE_INSTALL_ROOT      e.g., /work/y07/shared/
 # TEMPLATE_METIS_VERSION     5.1.0
 
@@ -101,15 +101,18 @@ if { ! [ info exists env(PE_ENV) ] } {
     prepend-path PATH $product_curpath/bin
     prepend-path LD_LIBRARY_PATH $product_curpath/lib
 
-    # load or remove paths for CRAY_LD_LIBRARY_PATH
+    # METIS_DIR also fragile
+    setenv ${ucName}_DIR $product_curpath
+
+    # load or remove paths for CSE_LD_LIBRARY_PATH
     if { ! [ module-info mode remove ] } {
-      prepend-path CRAY_LD_LIBRARY_PATH $product_curpath/lib
+      prepend-path CSE_LD_LIBRARY_PATH $product_curpath/lib
     } else {
-      set oldpath $env(CRAY_LD_LIBRARY_PATH)
+      set oldpath $env(CSE_LD_LIBRARY_PATH)
       foreach mod [ split $oldpath ':' ] {
         if { [ expr [ lsearch [ split $mod '/' ] $lcName ] >= 0 ] } {
 	  # This is mode remove so prepend is remove
-          prepend-path CRAY_LD_LIBRARY_PATH $mod
+          prepend-path CSE_LD_LIBRARY_PATH $mod
         } 
       }
     }
