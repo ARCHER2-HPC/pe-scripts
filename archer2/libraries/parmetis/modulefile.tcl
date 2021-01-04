2e57c8cd0d8424bf56ba66261f72f20e114f9a45
Kevin Stratford <kevin@epcc.ed.ac.uk>
2021-01-04 15:08:58 +0000

    Add timestamp and remove LD PATHs

--- archer2/libraries/parmetis/modulefile.tcl
+++ archer2/libraries/parmetis/modulefile.tcl
@@ -1,12 +1,7 @@
 #%Module
 #
 
-# You must replace:
-# TEMPLATE_INSTALL_ROOT      e.g., /work/y07/shared/
-# TEMPLATE_PARMETIS_VERSION  4.0.3
-
-# whence libs are /work/y07/shared/libs/metis
-# modulefiles are /work/y07/shared/archer2-modules
+# This file was (semi-)automatically generated
 
 set shared_root TEMPLATE_INSTALL_ROOT
 set module_root ${shared_root}/archer2-modules
@@ -21,9 +16,10 @@ source ${module_root}/archer-pkgconfig-tcl.lib
 conflict parmetis
 
 proc ModulesHelp { } {
-  puts stderr "parmetis"
+  puts stderr "parmetis $::parmetis_version"
+  puts stderr "See https://docs.archer2.ac.uk/software-libraries/metis/"
   puts stderr "Installed by: Kevin Stratford, EPCC"
-  puts stderr "Date: December 2020\n"
+  puts stderr "Date: TEMPLATE_TIMESTAMP\n"
 }
 
 set _module_name  [module-info name]
@@ -100,22 +96,19 @@ if { ! [ info exists env(PE_ENV) ] } {
   } else {
     set product_curpath $pe_product_dir/$compiler/$gen_compiler
 
-    # Potential change while module is loaded not handled
-    prepend-path PATH $product_curpath/bin
-    prepend-path LD_LIBRARY_PATH $product_curpath/lib
+    # Potential compiler change while module is loaded not handled
 
-    # Also non-volatile
     setenv ${ucName}_DIR ${product_curpath}  
 
-    # load or remove paths for CSE_LD_LIBRARY_PATH
+    # load or remove paths
     if { ! [ module-info mode remove ] } {
-      prepend-path CSE_LD_LIBRARY_PATH $product_curpath/lib
+      prepend-path PATH $product_curpath/lib
     } else {
-      set oldpath $env(CSE_LD_LIBRARY_PATH)
+      set oldpath $env(PATH)
       foreach mod [ split $oldpath ':' ] {
         if { [ expr [ lsearch [ split $mod '/' ] $lcName ] >= 0 ] } {
 	  # This is mode remove so prepend is remove
-          prepend-path CSE_LD_LIBRARY_PATH $mod
+          prepend-path PATH $mod
         } 
       }
     }
