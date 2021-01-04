5436c75591d8f58e0ba69e24fec9f6c4b6ab1309
Kevin Stratford <kevin@epcc.ed.ac.uk>
2021-01-04 10:51:21 +0000

    Updated to integrate tests

--- archer2/libraries/metis/build-metis.sh
+++ archer2/libraries/metis/build-metis.sh
@@ -14,8 +14,6 @@ source ${script_root}/pkgconfig-lib.sh
 source ${script_root}/versions.sh
 source ${script_root}/command_line.sh
 
-source ${script_dir}/metis-test.sh
-
 function main {
 
     # Overall prefix must be supplied by command line
@@ -86,10 +84,10 @@ function metisBuild {
     metisPackageConfigFiles ${prefix}
 }
 
-# Remove any existing build directories
-
 function metisClean {
+
     rm -rf metis-${METIS_VERSION}
+
 }
 
 function metisBuildSerial {
@@ -129,12 +127,12 @@ function metisPackageConfigFiles {
     # pkgconfig files
     
     local prefix=${1}
-    local prgEnv=$(peEnvUpper)
+    local prgEnv=$(peEnvLower)
     
     declare -A pcmap
     pcmap[name]="metis"
     pcmap[version]=${METIS_VERSION}
-    pcmap[description]="metis library for ${prgEnv}"
+    pcmap[description]="metis library for ${prgEnv} compiler"
     pcmap[has_openmp]=1
     pcmap[extra_libs]=""
 
@@ -142,8 +140,11 @@ function metisPackageConfigFiles {
 	# AOCC requires -lm to ensure resolve math.h stuff
 	pcmap[extra_libs]="-lm"
     fi
-    
-    pcPackageConfigFiles ${prefix} pcmap
+
+    pcmap[requires]="metis_${prgEnv}"
+
+    pcRefactorPackageConfigFiles ${prefix} pcmap
+    pcFileWriteOverallPackageFile "${prefix}/lib/pkgconfig/metis.pc" pcmap
 }
 
 function metisInstallModuleFile {
@@ -167,6 +168,52 @@ function metisInstallModuleFile {
     module use ${module_dir}
     module load metis/${METIS_VERSION}
     module unload metis
+}
+
+function metisInstallationTest {
+
+    metisTest PrgEnv-cray
+    metisTest PrgEnv-gnu
+    metisTest PrgEnv-aocc
+
+    printf "Completed metis installation test successfully\n"
+}
+
+function metisTest {
+
+    local prgenv=${1}
+    local module_use=$(moduleInstallDirectory)
+    local graph_dir="graphs" # standard metis inputs
+
+    printf "Metis installation test for %s\n" "${prgenv}"
+
+    module -s restore ${prgenv}
+    module use ${module_use}
+
+    module load metis/${METIS_VERSION}
+
+    # Remove shared objects here
+    rm -f ${METIS_DIR}/lib/*so
+    
+    cd metis-${METIS_VERSION}
+
+    cp ${script_dir}/metis-install-test.c .
+    cc -fopenmp metis-install-test.c
+    ./a.out
+
+    cp ${script_dir}/metis*f90 .
+    ftn metis.f90 metis-install-test.f90
+    ./a.out
+
+    # Standalone utilities: gpmetis, mpmetis, ndmetis, m2gmetis
+
+    gpmetis ${graph_dir}/test.mgraph 4
+    gpmetis ${graph_dir}/4elt.graph 5
+    mpmetis ${graph_dir}/metis.mesh 8
+    ndmetis ${graph_dir}/copter2.graph 7
+    m2gmetis ${graph_dir}/metis.mesh /dev/null
+
+    cd -
 }
 
 main
